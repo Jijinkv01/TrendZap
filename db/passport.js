f8763f830fb94237d31d809ccddd6df97eaa269f
Jijinkv01 <jijinkv01@gmail.com>
2024-12-12 13:00:13 +0530

    Authentication Added

--- db/passport.js
+++ db/passport.js
@@ -7,7 +7,8 @@ const env=require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLINT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"https://trendzap.ddns.net/auth/google/callback"  
+    callbackURL:"https://trendzap.ddns.net/auth/google/callback"
+    
 },
 async (accessToken,refreshToken,profile,done)=>{
     try {
