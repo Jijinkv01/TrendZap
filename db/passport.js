9c07e008820657af63e9241d2e893c958bb81abc
Jijinkv01 <jijinkv01@gmail.com>
2024-12-12 13:09:27 +0530

    Auth changed to home

--- db/passport.js
+++ db/passport.js
@@ -7,7 +7,7 @@ const env=require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLINT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"https://trendzap.ddns.net/auth/google/callback"
+    callbackURL:"https://trendzap.ddns.net/home/auth/google/callback"
     
 },
 async (accessToken,refreshToken,profile,done)=>{
